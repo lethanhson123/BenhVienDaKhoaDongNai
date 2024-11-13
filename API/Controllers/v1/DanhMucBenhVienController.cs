1bf4368eec9be0d8179b852233968355cb27fb66
Administrator <Administrator@DON-TOOL>
2024-11-13 07:37:19 +0700

    20241112

--- API/Controllers/v1/DanhMucBenhVienController.cs
+++ API/Controllers/v1/DanhMucBenhVienController.cs
@@ -5,13 +5,29 @@
     [ApiVersion("1.0")]
     public class DanhMucBenhVienController : BaseController<DanhMucBenhVien, IDanhMucBenhVienService>
     {
-    private readonly IDanhMucBenhVienService _DanhMucBenhVienService;
-    private readonly IWebHostEnvironment _WebHostEnvironment;
-    public DanhMucBenhVienController(IDanhMucBenhVienService DanhMucBenhVienService, IWebHostEnvironment WebHostEnvironment) : base(DanhMucBenhVienService, WebHostEnvironment)
-    {
-    _DanhMucBenhVienService = DanhMucBenhVienService;
-    _WebHostEnvironment = WebHostEnvironment;
+        private readonly IDanhMucBenhVienService _DanhMucBenhVienService;
+        private readonly IWebHostEnvironment _WebHostEnvironment;
+        public DanhMucBenhVienController(IDanhMucBenhVienService DanhMucBenhVienService, IWebHostEnvironment WebHostEnvironment) : base(DanhMucBenhVienService, WebHostEnvironment)
+        {
+            _DanhMucBenhVienService = DanhMucBenhVienService;
+            _WebHostEnvironment = WebHostEnvironment;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("GetAll001ToListAsync")]
+        public virtual async Task<List<DanhMucBenhVien>> GetAll001ToListAsync()
+        {
+            List<DanhMucBenhVien> result = new List<DanhMucBenhVien>();
+            try
+            {
+                result = await _DanhMucBenhVienService.GetAllToListAsync();
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+            }
+            return result;
+        }
     }
-    }
-    }
+}
 
